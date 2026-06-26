37a480ec7dac12db34e1b1fcf6c1362118a8ac76
briantemedia <briantemedia@gmail.com>
2026-06-26 15:10:41 +0200

    Load fonts locally to stabilize layout

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import "@fontsource/cormorant-garamond/400.css";
+import "@fontsource/lora/400.css";
+import "@fontsource/lora/400-italic.css";
+import "@fontsource/oswald/400.css";
+import "@fontsource/oswald/500.css";
+import "@fontsource/oswald/600.css";
+import "@fontsource/oswald/700.css";
+import "@fontsource/sora/400.css";
+import "@fontsource/sora/500.css";
+import "@fontsource/sora/600.css";
+import "@fontsource/sora/700.css";
 import "./common.css";
 import "../style.css";
 
